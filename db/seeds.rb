4521268bb578144aeed757c1053cf21f8af22aee
Aashika Shetty <aashikavshetty@gmail.com>
2019-07-01 15:18:19 -0700

    as - Added image seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+image_list = [
+  'https://farm8.staticflickr.com/7568/15785724675_999435f19f_k.jpg',
+  'https://thenypost.files.wordpress.com/2018/10/102318-dogs-color-determine-disesases-life.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+  'http://absfreepic.com/absolutely_free_photos/small_photos/cute-hamster-with-yellow-hair-2516x1667_93382.jpg',
+  'https://i2.wp.com/franimals.org/wp-content/uploads/2016/04/harp-seal.jpg',
+  'https://peopledotcom.files.wordpress.com/2017/08/puppies-3.jpg'
+]
+
+image_list.each do |url|
+  Image.create(url: url)
+end
